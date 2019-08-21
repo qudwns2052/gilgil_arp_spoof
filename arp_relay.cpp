e6bb756c859bba18b425cb7c0425d1e692267972
root <root@localhost.localdomain>
2019-08-21 16:47:59 -0400

    190822

--- arp_relay.cpp
+++ arp_relay.cpp
@@ -5,18 +5,23 @@ void arp_relay(char * dev, pcap_t * handle, uint8_t ** Senders_IP, uint8_t ** Ta
     uint8_t My_MAC[6];
     uint8_t My_IP[4];
 
+    struct pcap_pkthdr* header;
+    const u_char* packet;
+
     /*        Get my IP and Mac          */
     GET_MY_IP_MAC(dev, My_IP, My_MAC);
 
     while (true)
     {
+
         printf("while~~~\n");
-        struct pcap_pkthdr* header;
-        const u_char* packet;
+//        struct pcap_pkthdr* header;
+//        const u_char* packet;
         int res = pcap_next_ex(handle, &header, &packet);
         if (res == 0) continue;
         if (res == -1 || res == -2) break;
 
+//        printf("%.3lf\n",clock()/(double)1000);
 
 
         ETH_header * eth_h = (ETH_header *)packet;
@@ -28,9 +33,10 @@ void arp_relay(char * dev, pcap_t * handle, uint8_t ** Senders_IP, uint8_t ** Ta
         //            continue;
         //        }
         int cnt = 1;
+
         while(cnt<=session_cnt)
         {
-            if(memcmp(Senders_MAC[cnt], eth_h->smac, 6) != 0)
+            if(!memcmp(Senders_MAC[cnt], eth_h->smac, 6))
                 cnt++;
             else
             {
@@ -53,8 +59,8 @@ void arp_relay(char * dev, pcap_t * handle, uint8_t ** Senders_IP, uint8_t ** Ta
         /******************Capture IP Packet********************/
 
         IP_header * ip_h = (IP_header *)(packet+ETHER_HEADER_SIZE);
-
         printf("Capture IP Packet\n");
+//        printf("%.3lf\n",clock()/(double)1000);
         size_t ip_SIZE = (ip_h->VHL & 0x0F) * 4;
         size_t total_SIZE = ntohs(ip_h->Total_LEN);
         TCP_header * tcp_h = (TCP_header *)(packet + ETHER_HEADER_SIZE + ip_SIZE);
@@ -63,8 +69,14 @@ void arp_relay(char * dev, pcap_t * handle, uint8_t ** Senders_IP, uint8_t ** Ta
         size_t payload_len = (total_SIZE) - (ip_SIZE + tcp_SIZE);
         size_t packet_SIZE = ETHER_HEADER_SIZE + total_SIZE;
 
+
         u_char* relay_packet = (u_char*)malloc(sizeof(u_char)*packet_SIZE);
+
+
         memcpy(relay_packet, packet, packet_SIZE);
+
+
+
         ETH_header * eth_relay_h = (ETH_header *)relay_packet;
 
         /*****************************************************/
@@ -72,12 +84,16 @@ void arp_relay(char * dev, pcap_t * handle, uint8_t ** Senders_IP, uint8_t ** Ta
 
         /*****************Send relay packet******************/
 
+
         memcpy(eth_relay_h->dmac, Targets_MAC[cnt], 6);
+
+
         memcpy(eth_relay_h->smac, My_MAC, 6);
+
 
         printf("send relay Packet...\n");
         pcap_sendpacket(handle, relay_packet, packet_SIZE);
-
+//        printf("%.3lf\n",clock()/(double)1000);
         /****************************************************/
 
         free(relay_packet);
